d0567c6115191cc7e9f30519ee8c03695427c5ae
bivarz <paulo.bivar@gmail.com>
2020-03-07 18:16:01 -0300

    Atualizando Schedule

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,3 +1,5 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -10,8 +12,19 @@ class ScheduleController {
     if (!checkUserProvider) {
       return res.status(401).json({ Error: 'User is not provider' });
     }
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
 
-    return res.json();
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: { [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)] },
+      },
+      order: ['date'],
+    });
+
+    return res.json({ appointments });
   }
 }
 
