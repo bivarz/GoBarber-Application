6a0aa40df8ccbffb2cea0d2095fee0c53d16df88
bivarz <paulo.bivar@gmail.com>
2020-03-12 17:47:03 -0300

    Adicionando Lib & Mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4e5982a0efbd00',
+    pass: '5b476b573bb63b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
